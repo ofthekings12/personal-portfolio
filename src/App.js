017c403dff1577b7d4bd812614793599b96dd707
Mikko De Los Reyes <mikkode212@gmail.com>
2022-03-22 19:49:22 -0400

    github icon on 'desktop'

--- src/App.js
+++ src/App.js
@@ -12,6 +12,15 @@ function App() {
          <div className='text'>
          <h1>Hi, I'm Mikko!</h1>
          </div>
+         <div className="github">
+         <img
+              src="http://pixelartmaker-data-78746291193.nyc3.digitaloceanspaces.com/image/d7e4e1e509c728d.png"
+              alt="github logo"
+              style={{ height: "100px" }}
+            />
+            Github
+         </div>
+
          <Navbar/>
          
       </ThemeProvider>
