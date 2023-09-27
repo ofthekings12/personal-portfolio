6654c9251d113daffc8ef12db9ec7210b1256ad5
Mikko De Los Reyes <mikkode212@gmail.com>
2023-09-27 10:08:42 -0400

    reverting back to original aboutme icon

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Navbar from "./components/Navbar/Navbar";
 import AboutWindow from "./components/Window/AboutWindow";
 import ProjectsWindow from "./components/Window/ProjectsWindow";
 import { Anchor } from "react95";
-import Aboutme from "./images/Aboutme.png";
+
 
 function App() {
   const [openAboutWindow, setOpenAboutWindow] = useState(false);
@@ -40,9 +40,9 @@ function App() {
           active={openAboutWindow}
         >
           <img
-            src={Aboutme}
+            src="https://www.pngkit.com/png/full/867-8677338_pixelated-pixelart-freetouse-doge-pixel-art-paint-icon.png"
             alt="about logo"
-            style={{ height: "90px" }}
+            style={{ height: "100px" }}
           />
           <div className="icon-label">About Me</div>
         </div>
