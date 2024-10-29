1216a4c3b5c0fbfa05ae762302d156f5ca9092fb
Mikko De Los Reyes <mikkode212@gmail.com>
2024-10-29 12:55:39 -0400

    added description for tip calculator in projects window

--- src/components/Window/Projects/TipCalculator.jsx
+++ src/components/Window/Projects/TipCalculator.jsx
@@ -1,11 +1,6 @@
 import { useState, useEffect } from "react";
-import {
-  Anchor,
-  Tooltip,
-  Fieldset,
-  LoadingIndicator,
-} from "react95";
-import './TipCalculator.css'
+import { Anchor, Tooltip, Fieldset, LoadingIndicator } from "react95";
+import "./TipCalculator.css";
 
 function TipCalculator() {
   const [loading, setLoading] = useState(false);
@@ -19,61 +14,66 @@ function TipCalculator() {
 
   return (
     <div className="tab">
-    <Fieldset className="fieldset">
-      {loading ? (
-        <div className="loading">
-          <p
-            style={{ textAlign: "center", marginBottom: "0.5rem" }}
-          >
-            Loading...
-          </p>
-          <LoadingIndicator loading={loading} />
-        </div>
-      ) : (
-        <>
-          <p className="redirect">
-            Click{" "}
-            <a
+      <Fieldset className="fieldset">
+        {loading ? (
+          <div className="loading">
+            <p style={{ textAlign: "center", marginBottom: "0.5rem" }}>
+              Loading...
+            </p>
+            <LoadingIndicator loading={loading} />
+          </div>
+        ) : (
+          <>
+            <p className="redirect">
+              Click{" "}
+              <a
+                href="https://github.com/ofthekings12/tip-out-calculator"
+                target="_blank"
+                rel="noreferrer"
+              >
+                HERE
+              </a>{" "}
+              to view this repository, click image to checkout LIVE demo
+            </p>
+            <Anchor
               href="https://github.com/ofthekings12/tip-out-calculator"
               target="_blank"
-              rel="noreferrer"
             >
-              HERE
-            </a>{" "}
-            to view this repository, click image to checkout LIVE
-            demo
-          </p>
-          <Anchor
-            href="https://github.com/ofthekings12/tip-out-calculator"
-            target="_blank"
-          >
-            <Tooltip
-              text="Click HERE to view this repository, click image to checkout LIVE demo"
-              enterDelay={100}
-              leaveDelay={500}
-            >
-              {" "}
-              <Anchor
-                href="https://kingwest-tip-calculator.netlify.app/"
-                target="_blank"
+              <Tooltip
+                text="Click HERE to view this repository, click image to checkout LIVE demo"
+                enterDelay={100}
+                leaveDelay={500}
               >
-                <img
-                  className="tip-calculator-img"
-                  src="https://github.com/ofthekings12/personal-portfolio/blob/main/src/assets/tipout-gif.gif?raw=true"
-                  alt="tip calculator demo"
-                />
-              </Anchor>
-            </Tooltip>
-          </Anchor>
-          <h2>Project Overview:</h2>
-          <p>
-         **Tip Calculator Description***
-          </p>
-        </>
-      )}
-    </Fieldset>
-  </div>
-  )
+                {" "}
+                <Anchor
+                  href="https://kingwest-tip-calculator.netlify.app/"
+                  target="_blank"
+                >
+                  <img
+                    className="tip-calculator-img"
+                    src="https://github.com/ofthekings12/personal-portfolio/blob/main/src/assets/tipout-gif.gif?raw=true"
+                    alt="tip calculator demo"
+                  />
+                </Anchor>
+              </Tooltip>
+            </Anchor>
+            <h2>Project Overview:</h2>
+            <p>
+              This Tip Out Calculator is a simple tool that helps streamline and
+              automate the calculation of tip outs in the service industry.
+              Instead of manually calculating your tip out—which can lead to
+              mistakes due to human error—this app does the heavy lifting for
+              you. I built this project after realizing that manual calculations
+              sometimes led to inaccurate tip outs, resulting in frustration
+              between staff and management. The app was developed based on a
+              step-by-step infographic created by a colleague that explained the
+              calculation process in detail.
+            </p>
+          </>
+        )}
+      </Fieldset>
+    </div>
+  );
 }
 
-export default TipCalculator
+export default TipCalculator;
--- src/components/Window/ProjectsWindow.js
+++ src/components/Window/ProjectsWindow.js
@@ -6,7 +6,7 @@ import {
   WindowContent,
   Tabs,
   Tab,
-  TabBody
+  TabBody,
 } from "react95";
 import "./ProjectsWindow.css";
 import Draggable from "react-draggable";
@@ -50,52 +50,54 @@ export default function ProjectsWindow({ handleClick }) {
         <WindowContent className="projects-window-content">
           <Tabs rows={2} value={activeTab} onChange={handleChange}>
             <Tab className="window-tab" value={0}>
-              <p className="window-tab">IR Health</p>
-            </Tab>
-            <Tab className="window-tab" value={1}>
-              <p className="window-tab">Kamika Consults</p>
-            </Tab>
-            <Tab className="window-tab" value={2}>
-              <p className="window-tab">Comments Section</p>
-            </Tab>
-            <Tab className="window-tab" value={3}>
-              <p className="window-tab">Product Card</p>
-            </Tab>
-            <Tab className="window-tab" value={4}>
-              <p className="window-tab">Up4Grabs</p>
-            </Tab>
-            <Tab className="window-tab" value={5}>
-              <p className="window-tab">RESOURCR.</p>
-            </Tab>
-            <Tab className="window-tab" value={6}>
-              <p className="window-tab">Covid Tracker</p>
-            </Tab>
-            <Tab className="window-tab" value={7}>
               <p className="window-tab">Tip Out Calculator</p>
             </Tab>
-            <Tab className="window-tab" value={8}>
+            <Tab className="window-tab" value={1}>
               <p className="window-tab">REST Countries</p>
+              </Tab>
+            <Tab className="window-tab" value={2}>
+              <p className="window-tab">IR Health</p>
             </Tab>
+            <Tab className="window-tab" value={3}>
+              <p className="window-tab">Kamika Consults</p>
+            </Tab>
+            <Tab className="window-tab" value={4}>
+              <p className="window-tab">Comments Section</p>
+            </Tab>
+            <Tab className="window-tab" value={5}>
+              <p className="window-tab">Product Card</p>
+            </Tab>
+            <Tab className="window-tab" value={6}>
+              <p className="window-tab">Up4Grabs</p>
+            </Tab>
+            <Tab className="window-tab" value={7}>
+              <p className="window-tab">RESOURCR.</p>
+            </Tab>
+            <Tab className="window-tab" value={8}>
+              <p className="window-tab">Covid Tracker</p>
+            </Tab>
+
+            
           </Tabs>
           <TabBody className="tabBody">
-            {/* IR Health Services */}
-            {activeTab === 0 && <IRHealth />}
-            {/* Kamika Consults */}
-            {activeTab === 1 && <KamikaConsults />}
-            {/* COMMENTS SECTION */}
-            {activeTab === 2 && <CommentsSection />}
-            {/* PRODUCT CARD */}
-            {activeTab === 3 && <ProductCard />}
-            {/* UP4GRABS */}
-            {activeTab === 4 && <Up4Grabs />}
-            {/* RESOURCR */}
-            {activeTab === 5 && <RESOURCR />}
-            {/* Covid Tracker */}
-            {activeTab === 6 && <CovidTracker />}
             {/* Tip Calculator */}
-            {activeTab === 7 && <TipCalculator />}
+            {activeTab === 0 && <TipCalculator />}
             {/* REST Countries */}
-            {activeTab === 8 && <RESTCountries/>}
+            {activeTab === 1 && <RESTCountries />}
+            {/* IR Health Services */}
+            {activeTab === 2 && <IRHealth />}
+            {/* Kamika Consults */}
+            {activeTab === 3 && <KamikaConsults />}
+            {/* COMMENTS SECTION */}
+            {activeTab === 4 && <CommentsSection />}
+            {/* PRODUCT CARD */}
+            {activeTab === 5 && <ProductCard />}
+            {/* UP4GRABS */}
+            {activeTab === 6 && <Up4Grabs />}
+            {/* RESOURCR */}
+            {activeTab === 7 && <RESOURCR />}
+            {/* Covid Tracker */}
+            {activeTab === 8 && <CovidTracker />}
           </TabBody>
         </WindowContent>
       </Window>
